3a33bcf1ffee63553b5c1485ab52cb1b00268d18
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2025-02-14 18:22:19 +0100

    Add temp_dir path for database testing

--- pipelines/tests/test_build_database.py
+++ pipelines/tests/test_build_database.py
@@ -3,9 +3,9 @@ from click.testing import CliRunner
 from pipelines.run import run_build_database
 
 
-def test_build_database_last_year():
+def test_build_database_last_year(tmp_path):
     runner = CliRunner()
-    with runner.isolated_filesystem():
+    with runner.isolated_filesystem(temp_dir=tmp_path):
         result = runner.invoke(run_build_database, ["--refresh-type", "last"])
 
         # TODO: Add SQL check to verify that the last year was processed
